870e28a5fe85b96edb47ba4d20fb4afa318ad79f
Igor Dranichnikov <i.dranichnikov@itech-group.ru>
2014-05-23 18:45:41 +0400

    Finish user delete, edit, update, index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,42 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+  
   def new
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+      
+    if current_user?(user)
+      flash[:error] = "You can not delete your profile."
+      redirect_to users_path
+    else
+      user.destroy
+      flash[:success] = "User successfully deleted."
+      redirect_to users_path
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile settings successfully edited!"
+      redirect_to @user
+    else
+      flash[:error] = "Edit attempt failed!"
+      render 'edit'
+    end
   end
 
   def create
@@ -21,11 +53,30 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.paginate(page: params[:page])
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
